de474a12d585a6d58aec07e174d2beac261182cc
Barry Wang <wjp_barry@hotmail.com>
2019-03-05 23:55:17 +0800

    .....nothing changes

--- SpreadSheetWorking/ViewModel/DaysOffListViewModel.cs
+++ SpreadSheetWorking/ViewModel/DaysOffListViewModel.cs
@@ -7,7 +7,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
 
 namespace SpreadSheetWorking.ViewModel
 {
@@ -23,11 +25,32 @@ namespace SpreadSheetWorking.ViewModel
             set { mymemlist = value; }
         }
 
+        private ObservableCollection<MemberInfo> basiclist;
+
+        public ObservableCollection<MemberInfo> BasicList
+        {
+            get { return basiclist; }
+            set { basiclist = value; }
+        }
+
+        private List<string> tempdataset;
+
+        public List<string> TempDataset
+        {
+            get { return tempdataset; }
+            set { tempdataset = value; }
+        }
+
 
         public DaysOffListViewModel()
         {
-            //mymemlist = Settings.MyMemCollection;
             mymemlist = SqlDBHelper.CommonMemList;
+            basiclist = mymemlist;
+            tempdataset = new List<string>();
+            foreach (var i in mymemlist)
+            {
+                tempdataset.Add(i.UserName);
+            };
         }
 
         public void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
@@ -35,16 +58,33 @@ namespace SpreadSheetWorking.ViewModel
             if (args.ChosenSuggestion != null)
             {
                 // User selected an item from the suggestion list, take an action on it here.
-                // Meet the problem. To filter data a view of the collection is needed. 
+                // Meet the problem. 
+                // Stop this: To filter data a view of the collection is needed. 
                 // So need to go back to binding and set collectionview source instead. Need some time. stop it right now.
-               
+              
             }
             else
             {
+                var querydata = args.QueryText;
+                var result = mymemlist.Where(c => c.Alias == querydata);
                 // Use args.QueryText to determine what to do.
             }
         }
 
+        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            // Only get results when it was a user typing,
+            // otherwise assume the value got filled in by TextMemberPath
+            // or the handler for SuggestionChosen.
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                //Set the ItemsSource to be your filtered dataset
+                //sender.ItemsSource = mymemlist;                        
+                sender.ItemsSource = tempdataset;               
+
+            }
+           
+        }
 
     }
 }
