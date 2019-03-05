05efa1b991495c8cd4b7606b605d5f75f480f6ef
Barry <wjp_barry@hotmail.com>
2019-03-05 17:52:07 +0800

    Working with collectionviewsource ing...

--- SpreadSheetWorking/ViewModel/DaysOffListViewModel.cs
+++ SpreadSheetWorking/ViewModel/DaysOffListViewModel.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace SpreadSheetWorking.ViewModel
 {
@@ -22,10 +23,26 @@ namespace SpreadSheetWorking.ViewModel
             set { mymemlist = value; }
         }
 
+
         public DaysOffListViewModel()
         {
             //mymemlist = Settings.MyMemCollection;
             mymemlist = SqlDBHelper.CommonMemList;
+        }
+
+        public void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            if (args.ChosenSuggestion != null)
+            {
+                // User selected an item from the suggestion list, take an action on it here.
+                // Meet the problem. To filter data a view of the collection is needed. 
+                // So need to go back to binding and set collectionview source instead. Need some time. stop it right now.
+               
+            }
+            else
+            {
+                // Use args.QueryText to determine what to do.
+            }
         }
 
 
